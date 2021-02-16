4d9d5c4656fc404467ca9abfb3c5a86fb3967215
RaulMtz074 <28.martinez.raul.1d@gmail.com>
2021-02-16 16:24:42 -0600

    Primera version

---
+++ src/practicagithub/PracticaGitHub.java
@@ -0,0 +1,15 @@
+package practicagithub;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Orange Range
+ */
+public class PracticaGitHub {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+    
+}
